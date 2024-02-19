520beb3ab34345d7971d57d87ba8724f25a31916
František Šilhán <frantisek@slhn.cz>
2024-02-19 15:28:44 +0100

    Add dynamic rendering of total items in Overview component

--- src/components/Overview.tsx
+++ src/components/Overview.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import Playground from './Playground';
 import Styles from '../styles/Overview.module.css';
+import { FeastContext } from '../providers/FeastProvider';
+import { useContext } from 'react';
 
 interface Props {
 }
 
 export const Overview: React.FC<Props> = () => {
+  const ctx = useContext(FeastContext);
   return (
     <div className={Styles["overview"]}>
-      <h1>Feast</h1>
+      <h1>{ctx.state.tables.reduce((tableSum, table) => tableSum + table.seats.reduce((seatSum, seat) => seatSum + seat.items, 0), 0)}</h1>
       <Playground />
     </div>
   );
